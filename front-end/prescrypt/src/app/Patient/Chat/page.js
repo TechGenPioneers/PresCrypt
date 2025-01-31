e83f3f77a8555c1a0eb8c9cca748b674da486e04
Nishara <dthnjayakody@gmail.com>
2025-02-01 00:20:40 +0530

    Update Chat

---
+++ front-end/prescrypt/src/app/Patient/Chat/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Components/footer/Footer";
+import Header from "../../Components/header/Header";
+import Layout from "../Chat/Layout";
+
+export default function Chat() {
+  return (
+    <div>
+      <Header />
+      <Layout />
+      <Footer />
+    </div>
+  );
+}
