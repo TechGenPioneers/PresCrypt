ec00817f18368e060c9e7d5bc895e01948fbfa38
Nishara <dthnjayakody@gmail.com>
2025-04-03 23:20:36 +0530

    feat: update appointments UI

---
+++ front-end/prescrypt/src/app/Doctor/Reports/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "../../Components/footer/Footer";
+import Sidebar from "../DoctorComponents/DoctorSidebar";
+import DateTimeDisplay from "../DoctorComponents/DateTimeDisplay"; 
+
+export default function page() {
+  const Title = "Generate Reports"; // Dynamic title for each page
+
+  return (
+    <div className="flex flex-col min-h-screen ml-32">
+      <div className="flex flex-grow">
+        <Sidebar />
+        <div className="flex-grow p-2 bg-[#D4E9EA] min-h-screen">
+          <div className="m-[0.1px] bg-white h-full w-full">
+            <DateTimeDisplay title={Title} /> {/* Pass the dynamic title */}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ front-end/prescrypt/src/app/Doctor/utils/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// axios instance 
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5213', //backend API URL
+    headers: {
+      'Content-Type': 'application/json',
+      
+    },
+  });
+  
+export default axiosInstance;
