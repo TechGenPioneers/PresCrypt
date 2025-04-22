14462608806386ba634aa4f31cf187c0ab7d1ae0
ravndu-dlshan <159864490+ravndu-dlshan@users.noreply.github.com>
2025-04-22 15:16:43 +0530

    feat:updated patient profile

--- front-end/prescrypt/src/app/page.jsx
+++ front-end/prescrypt/src/app/page.jsx
@@ -1,51 +1,24 @@
-// src/app/page.js
-import React from "react";
-import Header from "./Components/header/Header";
-import Footer from "./Components/footer/Footer";
-import styles from "./page.module.css";
-import Link from "next/link";  
+import React from 'react';
+import AdminNavBar from './Admin/AdminComponents/AdminNavBar';
+import AdminDashboard from './Admin/AdminComponents/AdminDashboard';
+import Footer from '@/app/Components/footer/Footer';
 
-
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <Header />
-      {/* Include the reusable Header component */}
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to PresCrypt</h1>
-        <p className={styles.description}>
-          A platform to manage your healthcare seamlessly.
-        </p>
-        <div className={styles.actions}>
-          {/* Use Next.js Link for routing */}
-          <Link href="/Patient/PatientProfile" passHref legacyBehavior>
-            <a className={styles.card}>
-              <h2>Profile &rarr;</h2>
-              <p>View and update your personal profile information.</p>
-            </a>
-          </Link>
-          <Link href="/Patient/PatientAppointments" passHref legacyBehavior>
-            <a className={styles.card}>
-              <h2>Appointments &rarr;</h2>
-              <p>Book, view, or manage your appointments.</p>
-            </a>
-          </Link>
-          <Link href="/Patient/Chat" passHref legacyBehavior>
-            <a className={styles.card}>
-              <h2>Chat &rarr;</h2>
-              <p>Communicate with your healthcare provider.</p>
-            </a>
-          </Link>
-          <Link href="/Patient/PatientDashboard" passHref legacyBehavior>
-  <a className={styles.card}>
-    <h2>Dashboard &rarr;</h2>
-    <p>Access your dashboard for quick updates.</p>
-  </a>
-</Link>
+const AdminDashboardPage = () => {
+    return (
+        <div>
+      <div className="flex">
+        <div className="w-27">
+          <AdminNavBar />
         </div>
-      </main>
-      <Footer />
+        <div className="w-full">
+          <AdminDashboard/>
+        </div>
+      </div>
+      <div>
+          <Footer />
+      </div>
     </div>
-  );
-}
+    );
+};
+
+export default AdminDashboardPage;
