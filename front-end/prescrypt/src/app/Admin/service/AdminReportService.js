f598f7d7034ca9985fcffe358019950c0053a8b1
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-04-29 00:42:59 +0530

    feat:Admin report generation

--- front-end/prescrypt/src/app/Admin/service/AdminReportService.js
+++ front-end/prescrypt/src/app/Admin/service/AdminReportService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const ReportUrl = "https://localhost:7021/api/AdminReport"
 
 const GetAllDetails = async () => {
-    //get the Details
+    //get the all doctors patients and specialty Details
        try{
          const response = await  axios.get(`${ReportUrl}/GetAll`)
          return response.data
@@ -13,4 +13,16 @@ const GetAllDetails = async () => {
        }
  }
 
- export{GetAllDetails}
+ const GetReportDetails = async (reportDetails) => {
+  try {
+    const response = await axios.post(ReportUrl, reportDetails);
+    console.log("Report Details Response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to get the data", error);
+    throw error;
+  }
+};
+
+
+ export{GetAllDetails,GetReportDetails}
