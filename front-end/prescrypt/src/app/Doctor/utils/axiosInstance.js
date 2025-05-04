b97049b450e8fa15e2ce590ee6cd0ff4f545636b
Nishara <dthnjayakody@gmail.com>
2025-05-04 18:05:19 +0530

    feat: add report generation feature for doctors

--- front-end/prescrypt/src/app/Doctor/utils/axiosInstance.js
+++ front-end/prescrypt/src/app/Doctor/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios instance 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5213/api', //backend API URL
+    baseURL: 'http://localhost:7021/api', //backend API URL
     headers: {
       'Content-Type': 'application/json',
       
